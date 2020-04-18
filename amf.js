25e102f35eb70e4b71568ac0d6827d9501584814
SwampApe <rothvann2000@gmail.com>
2020-04-18 14:42:20 -0700

    Added AMF0 and AMF3 type markers

---
+++ amf.js
@@ -0,0 +1,38 @@
+
+
+
+const AMF0 = {
+    NUMBER: 0x00,
+    BOOLEAN: 0x01,
+    STRING: 0x02,
+    OBJECT: 0x03,
+    NULL: 0x05,
+    ECMA_ARRAY: 0x08,
+    OBJECT_END: 0x09,
+    STRICT_ARRAY: 0x0a,
+    DATE: 0x0b,
+    LONG_STRING: 0x0c,
+    XML: 0X0f,
+    TYPED_OBJECT: 0x10,
+    AMF3: 0x11    
+}
+
+const AMF3 = {
+    UNDEFINED: 0x00,
+    NULL: 0x01,
+    FALSE: 0x02,
+    TRUE: 0x03,
+    INTEGER: 0x04,
+    DOUBLE: 0x05,
+    STRING: 0x06,
+    XML: 0x07,
+    DATE: 0x08,
+    ARRAY: 0x09,
+    OBJECT: 0x0a,
+    XML_END: 0x0b,
+    BYTE_ARRAY: 0x0c
+}
+
+function decode() {
+    
+}
