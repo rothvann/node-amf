ebbe04f32c9b9ae2984453e53844ef2325ab39e4
SwampApe <rothvann2000@gmail.com>
2020-04-27 15:10:55 -0700

    AMF3 types added but not yet tested

--- amf.js
+++ amf.js
@@ -49,20 +49,11 @@ class AMF {
             this.position++;
             
             switch(type) {
-                case this.AMF0.NUMBER:
-                case this.AMF0.BOOLEAN:
-                case this.AMF0.STRING:   
-                case this.AMF0.NULL:
-                case this.AMF0.LONG_STRING:
-                case this.AMF0.XML:
-                case this.AMF0.DATE:
-                    let val = this.readAMF0Value(buf, type);
-                    result[key] = val;
-                    break;
                 case this.AMF0.OBJECT_END:
                     return result;
                 default:
-                    return result;
+                    let val = this.readAMF0Value(buf, type);
+                    result[key] = val;
             }
         }
     }
@@ -94,13 +85,13 @@ class AMF {
             }.replace(/\s/g, '');
             case this.AMF0.DATE:
                 this.position += 10;
-                console.log(buf.readDoubleBE(this.position - 8));
                 return new Date(buf.readDoubleBE(this.position - 8));
             case this.AMF0.AMF3: 
                 this.is_Ver_0 = false;
                 return;            
             default:
-                //unexpected;
+                console.log('Unknown AMF0 Value ${type}');
+                return undefined;
         }
     }
 
@@ -111,63 +102,176 @@ class AMF {
         for(let i = 0; i < length; i++) {
             let type = buf[this.position];
             this.position++;
-            switch(type) {
-                case this.AMF0.NUMBER:
-                case this.AMF0.BOOLEAN:
-                case this.AMF0.STRING:   
-                case this.AMF0.NULL:
-                case this.AMF0.LONG_STRING:
-                case this.AMF0.XML:
-                case this.AMF0.DATE:
-                    result.push(this.readAMF0Value(buf, type));
-                    break;
-                default:
-                    //wuhh
-                    return result;
-            }
+            result.push(this.readAMF0Value(buf, type));
         }
         return result;
+    }
+    
+    readAMF3Array(buf) {
+        let length = this.readU29(buf);
+        if(length & 1 == 0) {
+            let index = length >> 1;
+        } else {
+            length = length >> 1;
+            let dense = [];
+            for(let i = 0; i < length; i++) {
+                let type = buf[this.position];
+                this.position++;
+                dense.push(this.readAMF3Value(buf, type));
+            }
+            let associative = {};
+            while(true) {
+                let key = this.readAMF3Value(buf, this.AMF3.STRING);
+                if(key == '') {
+                    return {dense: dense, associative: associative};
+                } else {
+                    let type = buf[this.position];
+                    this.position++;
+                    associative[key] = this.readAMF3Value(buf, type);
+                }
+            }
+        }
+    }
+    
+    readAMF3Object(buf) {
+        let flags = this.readU29(buf);
+        let traits;
+        if(flags & 0b1 == 0) {
+            //U29O-ref
+            let index = flags >> 1;
+            //return ref
+            
+        } else {
+            if(flags & 0b10 == 0) {
+                let index = flags >> 1;
+                
+                //set traits from ref
+                
+            } else {=
+                if(flags & 0b100 == 0) {
+                    if(flags & 0b1000 == 0) {
+                        traits = 0b00;
+                    } else {
+                        traits = 0b10;
+                    }
+                } else {
+                    traits = 0b01;
+                }
+            }
+        }
+        
+        if(traits & 1 == 0) {
+            let length = flags >> 4;
+            let result = {};
+            
+            let name = this.readAMF3Value(buf, this.AMF3.STRING);
+            result['name'] = name;
+            
+            
+            for(let i = 0; i < length; i++) {
+                let key = this.readAMF3Value(buf, this.AMF3.STRING);
+                let type = buf[this.position];
+                this.position++;
+                result[key] = this.readAMF3Value(buf, type);
+            }
+            
+            
+           
+            if(traits & 0b10 != 0) {
+                while(true) {
+                    let key = this.readAMF3Value(buf, this.AMF3.STRING);
+                    if(key == '') {
+                        return result;
+                    }
+                    let type = buf[this.position];
+                    this.position++;
+                    result[key] = this.readAMF3Value(buf, type);
+                }
+            }
+            
+            return result;
+        } else {
+            //U29O-traits-ext
+        }
+            
+        
+        
+    }
+    
+    readU29(buf) {
+        let bits = 0;
+        let cur_byte = buf[this.position];
+        this.position++;
+        bits += (cur_byte & 0b01111111) * Math.pow(2, 24);
+        if(cur_byte & 0b10000000) {
+            cur_byte = buf[this.position];
+            this.position++;
+            bits += (cur_byte & 0b01111111) * Math.pow(2, 16);
+            if(cur_byte & 0b10000000) {
+                cur_byte = buf[this.position];
+                this.position++;
+                bits += (cur_byte & 0b01111111) * Math.pow(2, 8);
+                if(cur_byte & 0b10000000) {
+                    cur_byte = buf[this.position];
+                    this.position++;                        
+                    bits += cur_byte;
+                }
+            }
+        }
+        return bits;
     }
 
     readAMF3Value(buf, type) {
          switch(type) {
-             case this.AMF3.UNDEFINED:
+            case this.AMF3.UNDEFINED:
                 return undefined;
-             case this.AMF3.NULL:
+            case this.AMF3.NULL:
                 return null;
-             case this.AMF3.FALSE:
+            case this.AMF3.FALSE:
                 return false;
-             case this.AMF3.TRUE:
+            case this.AMF3.TRUE:
                 return true;
-             case this.AMF3.INTEGER:
-                let bits = 0;
-                let cur_byte = buf[this.position];
-                this.position++;
-                bits += (cur_byte & 0b01111111) * Math.pow(2, 24);
-                if(cur_byte & 0b10000000) {
-                    cur_byte = buf[this.position];
-                    this.position++;
-                    bits += (cur_byte & 0b01111111) * Math.pow(2, 16);
-                    if(cur_byte & 0b10000000) {
-                        cur_byte = buf[this.position];
-                        this.position++;
-                        bits += (cur_byte & 0b01111111) * Math.pow(2, 8);
-                        if(cur_byte & 0b10000000) {
-                            cur_byte = buf[this.position];
-                            this.position++;                        
-                            bits += cur_byte;
-                        }
-                    }
+            case this.AMF3.INTEGER:
+                return this.readU29(buf);
+            case this.AMF3.DATE:
+                let length = readU29(buf);
+                if(length & 1 == 0) {
+                    let index = length >> 1;
+                    //date ref
                 }
-                return bits;
-             case this.AMF3.DOUBLE:
-                
-             case this.AMF3.STRING:
-                
-             
+            case this.AMF3.DOUBLE:
+                this.position += 8;
+                return new Date(buf.readDoubleBE(this.position - 8));
+            case this.AMF3.STRING:
+            case this.AMF3.XMLDocument:
+            case this.AMF3.XML:
+                let length = this.readU29(buf);
+                if(length & 1 == 0) {
+                    let index = length >> 1;
+                    //string ref
+                } else {
+                    length = length >> 1;
+                    let key = buf.toString('utf8', this.position, this.position + length);
+                    this.position += length;
+                    return key;
+                }
+                else {
+                    this.position += 8;
+                    return new Date(buf.readDoubleBE(this.position - 8));
+                }
+            case this.AMF3.BYTE_ARRAY:
+                let length = this.readU29(buf);
+                if(length & 1 == 0) {
+                    let index = length >> 1;
+                    //byte array ref
+                } else {
+                    length = length >> 1;
+                    return buf.slice(this.position, this.position + length);
+                }
+            default:
+                console.log('Unknown AMF3 Value ${type}');
+                return undefined;
          }
-        
-        
     }
     
     decode(buf) {
@@ -178,14 +282,6 @@ class AMF {
         this.position++;
         if(this.is_Ver_0) {
             switch(type) {
-                case this.AMF0.NUMBER:
-                case this.AMF0.BOOLEAN:
-                case this.AMF0.STRING:   
-                case this.AMF0.NULL:
-                case this.AMF0.LONG_STRING:
-                case this.AMF0.XML:
-                case this.AMF0.DATE:
-                    return this.readAMF0Value(buf, type);
                 case this.AMF0.AMF3:
                     this.readAMF0Value(buf, type);
                 case this.AMF0.TYPED_OBJECT:
@@ -200,13 +296,17 @@ class AMF {
                     return this.decodeAMF0Obj(buf);
                 case this.AMF0.STRICT_ARRAY:
                     return this.readAMF0StrictArray(buf);
+                default:
+                    return this.readAMF0Value(buf, type);
             }
         } else {
             switch(type) {
-                
-                
-                
-                
+             case this.AMF3.ARRAY:
+                return this.readAMF3Array(buf);
+             case this.AMF3.OBJECT:
+                return this.readAMF3Object(buf);
+             default:
+                return this.readAMF3Value(buf, type);
             }
         }
         
